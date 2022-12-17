b66a4645d947ee29f5ab254107cf42b7a57cc827
oeyoews <jyao4783@gmail.com>
2022-12-17 11:02:16 +0800

    Update get-site-map.ts

--- lib/get-site-map.ts
+++ lib/get-site-map.ts
@@ -9,7 +9,7 @@ import { getCanonicalPageId } from './get-canonical-page-id'
 import { notion } from './notion-api'
 
 const uuid = !!includeNotionIdInUrls
-const cache = new ExpiryMap(10000)
+const cache = new ExpiryMap(5000)
 
 export async function getSiteMap(): Promise<types.SiteMap> {
   const partialSiteMap = await getAllPages(
