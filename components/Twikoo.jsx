56162056d918ff3c28f4d3f46fbcf67f112b9cf5
oeyoews <jyao4783@gmail.com>
2022-12-30 20:27:38 +0800

    Automatic commit

--- components/Twikoo.jsx
+++ components/Twikoo.jsx
@@ -5,6 +5,7 @@ import twikoo from 'twikoo'
 const Twikoo = () => {
   useEffect(() => {
     twikoo({
+      // WIP
       envId: 'https://twikoo-oeyoews.vercel.app', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
       el: '#twikoo', // 容器元素
       lang: 'en' // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
