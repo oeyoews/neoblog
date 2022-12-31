b571add3ad6cecdcae4cc92dbc2581ca07815539
oeyoews <jyao4783@gmail.com>
2023-01-01 00:02:12 +0800

    feat: mark author field

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,9 @@ export const FooterImpl: React.FC = () => {
   return (
     <>
       <footer className={styles.footer}>
-        <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+        <div className={styles.copyright}>
+          Copyright 2022 <mark>{config.author}</mark>
+        </div>
 
         <div className={styles.settings}>
           {hasMounted && (
