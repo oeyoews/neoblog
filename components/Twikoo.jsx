7dfef9a23ddce0e180463d7c652f2b701a75467b
oeyoews <jyao4783@gmail.com>
2023-02-13 17:08:19 +0800

    Automatic commit

--- components/Twikoo.jsx
+++ components/Twikoo.jsx
@@ -8,7 +8,7 @@ const Twikoo = () => {
     twikoo({
       // WIP
       // envId: 'https://twikoo-oeyoews.vercel.app', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
-      envId: 'https://oeyoews.fun', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+      envId: 'https://twikoo.oeyoewl.top', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
       el: '#twikoo', // 容器元素
       lang: 'en' // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
       // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
