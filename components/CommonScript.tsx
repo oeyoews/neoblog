2da0fa75f75e881c6502e1353dea34d57cc08f6d
oeyoews <jyao4783@gmail.com>
2023-03-27 21:58:56 +0800

    Automatic commit

--- components/CommonScript.tsx
+++ components/CommonScript.tsx
@@ -12,18 +12,15 @@ const CommonScript = () => {
         //   src='https://code.tidio.co/4wfemkcvx76wrofc8n1rpz8pxqrihxae.js'
         //   async
         // ></script>
+        // <script
+        //   async
+        //   src='https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js'
+        // />
+        // <script
+        //   async
+        //   src='https://cdn.jsdelivr.net/gh/oeyoews/MetingJS@master/source/Meting.js'
+        // />
       }
-      {/* music */}
-      /*{' '}
-      <script
-        async
-        src='https://cdnjs.cloudflare.com/ajax/libs/aplayer/1.10.1/APlayer.min.js'
-      />
-      <script
-        async
-        src='https://cdn.jsdelivr.net/gh/oeyoews/MetingJS@master/source/Meting.js'
-      />{' '}
-      */
     </>
   )
 }
