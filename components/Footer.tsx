cd4b36029eb6dea8d64843e25288e88c8eadaafc
oeyoews <jyao4783@gmail.com>
2023-04-01 19:47:44 +0800

    Automatic commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -29,8 +29,8 @@ export const FooterImpl: React.FC = () => {
     <>
       <footer className={styles.footer}>
         <div className={styles.copyright}>
-          {/* maybe have some question https://github.com/transitive-bullshit/nextjs-notion-starter-kit/pull/432 */}
-          /* Copyright {config.author} */
+          {/* maybe have some question https://github.com/transitive-bullshit/nextjs-notion-starter-kit/pull/432 */
+          /* Copyright {config.author} */}
         </div>
         <div className={styles.settings}>
           {hasMounted && (
